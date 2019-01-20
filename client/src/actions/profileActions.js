3de463dd47b8e940533f6549d5988f4e4499a76e
younes assami <assami.younes@gmail.com>
2019-01-19 22:00:40 -0500

    Completed create profile, logged in user can enter his profile information and submit it

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,9 +2,22 @@ import axios from "axios";
 import {
   GET_PROFILE,
   PROFILE_LOADING,
-  GET_ERRORS,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS
 } from "./types";
+
+// create profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 // get current profile
 export const getCurrentProfile = () => dispatch => {
