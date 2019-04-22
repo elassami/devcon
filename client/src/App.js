7d31148867da21ff8cdb090993dd0bdd2fed8e9f
younes assami <assami.younes@gmail.com>
2019-04-22 15:43:05 -0400

    Add github repos list to the profile page. Finished profile functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
-
+import NotFound from "./components/not-found/NotFound";
 import "./App.css";
 
 //check for token
@@ -88,6 +88,7 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
