ed795d5ddac5dd2b30afcd547caa93080d8b8d4f
Younes El Assami <assami.younes@gmail.com>
2019-04-27 13:22:38 -0400

    Added posts, like, unlike functionality

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS } from "./types";
+import {
+  ADD_POST,
+  GET_ERRORS,
+  CLEAR_ERRORS,
+  GET_POSTS,
+  GET_POST,
+  POST_LOADING,
+  DELETE_POST
+} from "./types";
 
 // Add post
 export const addPost = postData => dispatch => {
+  //dispatch(clearErrors());
   axios
-    .post("api/posts", postData)
+    .post("/api/posts", postData)
     .then(res =>
       dispatch({
         type: ADD_POST,
@@ -18,4 +27,69 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,9 @@
-import addPost, { ADD_POST } from "../actions/types";
+import addPost, {
+  ADD_POST,
+  GET_POSTS,
+  DELETE_POST,
+  POST_LOADING
+} from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -8,10 +13,27 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+
     case ADD_POST:
       return {
         ...state,
         posts: [action.payload, ...state.posts]
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
       };
     default:
       return state;
